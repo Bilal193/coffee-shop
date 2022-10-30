393e80f7dec97ce57b6d1b3c125f89b18dbb0004
Bilal <bilalarshad.me38@gmail.com>
2022-10-30 13:02:41 +0500

    Testing git branch features

--- src/components/Darkcoffee.js
+++ src/components/Darkcoffee.js
@@ -32,21 +32,12 @@ const Darkcoffee = () => {
 
 
                             <p className='pe-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum blanditiis, dolorum praesentium natus cum nesciunt ad ipsum esse ab culpa quae ipsa sed possimus iure eaque unde explicabo consectetur necessitatibus animi omnis, alias totam. Commodi, quia ducimus amet quasi officiis saepe at dicta nobis aut iusto modi corporis sit quae. Odio blanditiis error explicabo voluptas, officia quidem incidunt eius natus eveniet nobis. Voluptatum repudiandae labore sapiente obcaecati ut velit alias vitae facere? Soluta maxime dolores quidem aspernatur repellat. Nam cupiditate quisquam magni earum explicabo a iste nisi aliquid inventore, adipisci, culpa tempore maxime suscipit, modi ratione. Quisquam totam vero itaque.</p>
-
                         </div>
-
-
                     </div>
-
-
-
                 </div>
-                {/* <div className="col-4 pe-5 ps-0 text-end text-white m-top-text circle-to-wrap ">
-                <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam aperiam officiis ipsa assumenda quia libero facere amet dolor, autem ipsam quam? Labore voluptas vel ex tempora, non molestiae magni inventore ad eius nihil vero quod, nobis quam suscipit reprehenderit consequatur. Enim, ad. Dolorem in ipsa, sequi nihil asperiores illo harum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quod, dolore delectus id libero excepturi neque repudiandae temporibus aliquid voluptas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A neque incidunt nam hic accusamus. Nulla a consequatur doloribus quo officiis?</p>
-            </div> */}
             </div>
             <div className="row spacer-2">
-
+this text is written in working branch and should not exist in the main branch
             </div>
         </div>
     )
